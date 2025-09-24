3742043a0333b36097db96d72bcca077684fff6b
Enderbyte09 <enderbyte09@gmail.com>
2025-09-24 07:24:10 -0700

    del old code

--- src/main/java/enderbyteprograms/actionspeed/ActionSpeedData.java
+++
@@ -1,8 +0,0 @@
-package enderbyteprograms.actionspeed;
-import java.util.HashMap;
-
-public class ActionSpeedData {
-
-    public static HashMap<String,PlayerData> Players = new HashMap<>();//This dict keeps track of players and their player data
-    public static String version = "1.7.1";
-}
