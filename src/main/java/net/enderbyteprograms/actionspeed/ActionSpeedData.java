0f349de4e6946183572ee341431eb488f57ea8d8
Jordan Rahim <enderbyte09@gmail.com>
2026-01-20 19:35:00 -0800

    1.9.1

--- src/main/java/net/enderbyteprograms/actionspeed/ActionSpeedData.java
+++ src/main/java/net/enderbyteprograms/actionspeed/ActionSpeedData.java
@@ -8,7 +8,7 @@ public class ActionSpeedData {
 
     public static HashMap<String,PlayerData> Players = new HashMap<>();//This dict keeps track of players and their player data
     //public static HashMap<String, Location> Playerlocs = new HashMap<>();
-    public static String version = "1.9";
+    public static String version = "1.9.1";
     public static Table MainDataTable;
     public static double SampleRate = 10;//Hertz
     public static long TickRate = 2;
--- src/main/java/net/enderbyteprograms/actionspeed/SpeedLooper.java
+++ src/main/java/net/enderbyteprograms/actionspeed/SpeedLooper.java
@@ -10,40 +10,47 @@ public class SpeedLooper extends BukkitRunnable {
     @Override
     public void run() {
         for (Player player:Bukkit.getOnlinePlayers()) {
-            if (!ActionSpeedData.Players.containsKey(player.getUniqueId().toString())) {
-                //Ignore it for now and wait for join event handler to finish up
-            } else {
-                PlayerData assocdata = ActionSpeedData.Players.get(player.getUniqueId().toString());
-                if (!assocdata.active) {
-                    continue;//Don't waste time calculating for those who are not active
-                }
-                Location l = player.getLocation();
-                if (assocdata.lastMoveLocation == null) {
-                    assocdata.SendActionBarMessage("0 "+assocdata.unitstr);
-                    assocdata.lastMoveLocation = l;
+            try {
+                if (!ActionSpeedData.Players.containsKey(player.getUniqueId().toString())) {
+                    //Ignore it for now and wait for join event handler to finish up
                 } else {
-                    Location previouslocation = assocdata.lastMoveLocation;
-                    if (previouslocation.equals(l)) {
-                        //User has not moved
-                        if (ActionSpeedMain.CONFIG.getBoolean("showzeros")) {
-                            assocdata.SendActionBarMessage("0 "+assocdata.unitstr);
-                        }
-                    } else {
-                        double dist;
-                        try {
-                            dist = l.distance(previouslocation);
-                        } catch (Exception e) {
-                            dist = 0;
-                        }
+                    PlayerData assocdata = ActionSpeedData.Players.get(player.getUniqueId().toString());
+                    if (assocdata == null) {
+                        continue;//Stop the spam!
+                    }
+                    if (!assocdata.active) {
+                        continue;//Don't waste time calculating for those who are not active
+                    }
+                    Location l = player.getLocation();
+                    if (assocdata.lastMoveLocation == null) {
+                        assocdata.SendActionBarMessage("0 " + assocdata.unitstr);
                         assocdata.lastMoveLocation = l;
-                        dist *= ActionSpeedData.SampleRate;//Fired 10 times per second
-                        ChatColor tcol = Utilities.speedColour(dist,assocdata);
-                        double converteddist = Utilities.round(Utilities.convertspeed(assocdata.unit,dist),1);
-                        String finalstr = String.valueOf(converteddist) + " " + assocdata.unitstr;
+                    } else {
+                        Location previouslocation = assocdata.lastMoveLocation;
+                        if (previouslocation.equals(l)) {
+                            //User has not moved
+                            if (ActionSpeedMain.CONFIG.getBoolean("showzeros")) {
+                                assocdata.SendActionBarMessage("0 " + assocdata.unitstr);
+                            }
+                        } else {
+                            double dist;
+                            try {
+                                dist = l.distance(previouslocation);
+                            } catch (Exception e) {
+                                dist = 0;
+                            }
+                            assocdata.lastMoveLocation = l;
+                            dist *= ActionSpeedData.SampleRate;//Fired 10 times per second
+                            ChatColor tcol = Utilities.speedColour(dist, assocdata);
+                            double converteddist = Utilities.round(Utilities.convertspeed(assocdata.unit, dist), 1);
+                            String finalstr = String.valueOf(converteddist) + " " + assocdata.unitstr;
 
-                        assocdata.SendActionBarMessage(tcol+finalstr+ChatColor.RESET);
+                            assocdata.SendActionBarMessage(tcol + finalstr + ChatColor.RESET);
+                        }
                     }
                 }
+            } catch (Exception e) {
+                //Do nothing
             }
         }
     }
